48c777be4a62d1f6ad7b4ab0e9e1eff8c0e59859
Sacha Brants <sbrants-menard@slb.com>
2017-06-06 14:34:43 -0700

    a few cosmetic things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GeojsonService } from './geojson.service';
+import { LatLngLiteral } from '@agm/core/services/google-maps-types';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import { GeojsonService } from './geojson.service';
 })
 export class AppComponent implements OnInit {
   zoom = 4;
-  center = {lat: -28, lng: 137};
+  center: LatLngLiteral = {lat: -28, lng: 137};
   errorMessage: string;
   geoJsonObject: object;
 
