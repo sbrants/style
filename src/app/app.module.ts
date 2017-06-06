9c483cabac4723ba4ab4ddd7cb94b0ccca4d0565
Sacha Brants <sbrants-menard@slb.com>
2017-06-06 11:06:15 -0700

    Working style with local GeoJson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+import { GeojsonService } from './geojson.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR_KEY'
+    })
   ],
-  providers: [],
+  providers: [GeojsonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
